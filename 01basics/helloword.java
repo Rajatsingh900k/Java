d4dde0cdf137fd82f84753f488e9237321e99b77
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-17 21:52:18 +0530

    Add helloword.java with basic Hello World program and age display

---
+++ 01basics/helloword.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class helloword{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println(7+4);
+        age();
+    }
+    public static void age(){
+        int age=23;
+        System.out.printf("My age is ");
+        System.out.println(age);
+    }
+}
